623fbb0553a978a1ae0af014e545f8e9c3025e9e
Crimix <christofferm@live.dk>
2018-04-01 16:27:12 +0200

    - Increase ranges across the board. - Fixed that the keybind was missing to unequip the belt.

--- src/main/java/com/black_dog20/itemgrabber/config/Server.java
+++ src/main/java/com/black_dog20/itemgrabber/config/Server.java
@@ -11,8 +11,8 @@ public class Server {
 	
 	@Config.Name("Magnet T1 Range")
 	@Config.LangKey("gadgetronig.config.range")
-	@Config.RangeInt(min = 5, max = 15)
-	public int rangeT1 = 5;
+	@Config.RangeInt(min = 5, max = 20)
+	public int rangeT1 = 10;
 	
 	@Config.Name("Magnet T2 Speed")
 	@Config.LangKey("gadgetronig.config.speed")
@@ -21,6 +21,6 @@ public class Server {
 	
 	@Config.Name("Magnet T2 Range")
 	@Config.LangKey("gadgetronig.config.range")
-	@Config.RangeInt(min = 5, max = 15)
-	public int rangeT2 = 10;
+	@Config.RangeInt(min = 10, max = 25)
+	public int rangeT2 = 15;
 }
