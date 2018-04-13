e7990804337be564f4d8a598138ee0884f45e46e
Crimix <christofferm@live.dk>
2018-04-13 17:46:54 +0200

    - Update gradle  - Fix such that the block gets dropped

--- src/main/java/com/black_dog20/itemgrabber/block/BlockBase.java
+++ src/main/java/com/black_dog20/itemgrabber/block/BlockBase.java
@@ -9,6 +9,7 @@ import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
@@ -24,6 +25,7 @@ public class BlockBase extends Block implements IItemModelRegister{
 		setUnlocalizedName(getRegistryName().toString());
 		registerItemForm();
 		setCreativeTab(CreativeTabGTIG.TAB);
+		setHardness(1.0F);
 	}
 	
 	public void registerItemForm() {
