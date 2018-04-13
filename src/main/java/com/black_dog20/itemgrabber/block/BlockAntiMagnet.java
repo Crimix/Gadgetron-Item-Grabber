8620c31ee7f60a97f82fbcd293b93758ce43c1e2
Crimix <christofferm@live.dk>
2018-04-13 17:46:54 +0200

    - Update gradle  - Fix such that the block gets dropped

--- src/main/java/com/black_dog20/itemgrabber/block/BlockAntiMagnet.java
+++ src/main/java/com/black_dog20/itemgrabber/block/BlockAntiMagnet.java
@@ -3,26 +3,20 @@ package com.black_dog20.itemgrabber.block;
 import com.black_dog20.itemgrabber.reference.AntiType;
 import com.black_dog20.itemgrabber.tileentity.TileEntityAntiMagnet;
 
-import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 
-public class BlockAntiMagnet extends BlockBase implements ITileEntityProvider{
+public class BlockAntiMagnet extends BlockBase{
 
 	private AntiType type;
 	private int range;
 	
 	public BlockAntiMagnet(String name, AntiType type, int range) {
-		super(Material.ROCK, name);
+		super(Material.WOOD, name);
 		this.type = type;
 		this.range = range;
-	}
-
-	@Override
-	public TileEntity createNewTileEntity(World worldIn, int meta) {
-		return new TileEntityAntiMagnet(type, (range+1));
 	}
 	
 	@Override
@@ -34,4 +28,6 @@ public class BlockAntiMagnet extends BlockBase implements ITileEntityProvider{
 	public TileEntity createTileEntity(World world, IBlockState state) {
 		return new TileEntityAntiMagnet(type, (range+1));
 	}
+	
+	
 }
--- src/main/java/com/black_dog20/itemgrabber/block/BlockBase.java
+++ src/main/java/com/black_dog20/itemgrabber/block/BlockBase.java
@@ -9,9 +9,14 @@ import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.NonNullList;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 
 public class BlockBase extends Block implements IItemModelRegister{
 
@@ -21,6 +26,7 @@ public class BlockBase extends Block implements IItemModelRegister{
 		setRegistryName(new ResourceLocation(Reference.MOD_ID, name));
 		setUnlocalizedName(getRegistryName().toString());
 		setCreativeTab(CreativeTabGTIG.TAB);
+		setHardness(1.0F);
 	}
 
 	protected IBlockState pickDefaultState() {
