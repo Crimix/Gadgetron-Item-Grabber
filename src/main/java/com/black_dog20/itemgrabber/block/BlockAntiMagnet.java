66e2d0e228b9ac52a70bef134dc0d2cf09035626
Crimix <christofferm@live.dk>
2018-04-15 17:00:12 +0200

    - Organize imports

--- src/main/java/com/black_dog20/itemgrabber/block/BlockAntiMagnet.java
+++ src/main/java/com/black_dog20/itemgrabber/block/BlockAntiMagnet.java
@@ -3,13 +3,24 @@ package com.black_dog20.itemgrabber.block;
 import com.black_dog20.itemgrabber.reference.AntiType;
 import com.black_dog20.itemgrabber.tileentity.TileEntityAntiMagnet;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
 
 public class BlockAntiMagnet extends BlockBase{
 
+	public static final AxisAlignedBB BOUNDS = new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.0625D, 1.0D);
+	
 	private AntiType type;
 	private int range;
 	
@@ -28,6 +39,65 @@ public class BlockAntiMagnet extends BlockBase{
 	public TileEntity createTileEntity(World world, IBlockState state) {
 		return new TileEntityAntiMagnet(type, (range+1));
 	}
-	
-	
+
+    private boolean checkForIfItShouldDrop (World world, BlockPos pos, IBlockState state) {
+
+        if (!this.canBlockStay(world, pos)) {
+
+            this.dropBlockAsItem(world, pos, state, 0);
+            world.setBlockToAir(pos);
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+    @Override
+    public AxisAlignedBB getBoundingBox (IBlockState state, IBlockAccess source, BlockPos pos) {
+
+        return BOUNDS;
+    }
+
+    private boolean canBlockStay (World world, BlockPos pos) {
+
+        return !(world.isAirBlock(pos.down()) || !world.isSideSolid(pos.down(), EnumFacing.UP));
+    }
+
+    @Override
+    public boolean canPlaceBlockAt (World world, BlockPos pos) {
+
+        return super.canPlaceBlockAt(world, pos) && this.canBlockStay(world, pos);
+    }
+
+    @Override
+    public void neighborChanged (IBlockState state, World worldIn, BlockPos pos, Block blockIn, BlockPos fromPos) {
+
+        this.checkForIfItShouldDrop(worldIn, pos, state);
+    }
+
+    @Override
+    public AxisAlignedBB getCollisionBoundingBox (IBlockState blockState, IBlockAccess worldIn, BlockPos pos) {
+
+        return NULL_AABB;
+    }
+
+    @Override
+    public boolean isFullCube (IBlockState state) {
+
+        return false;
+    }
+
+    @Override
+    public boolean isOpaqueCube (IBlockState state) {
+
+        return false;
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public BlockRenderLayer getBlockLayer () {
+
+        return BlockRenderLayer.CUTOUT;
+    }
 }
