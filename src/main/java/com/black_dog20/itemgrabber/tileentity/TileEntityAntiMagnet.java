756e8173b9983249a05863ca6699d6608391508a
Crimix <christofferm@live.dk>
2018-07-21 20:15:44 +0200

    - Fixed it finally

--- src/main/java/com/black_dog20/itemgrabber/tileentity/TileEntityAntiMagnet.java
+++ src/main/java/com/black_dog20/itemgrabber/tileentity/TileEntityAntiMagnet.java
@@ -3,18 +3,25 @@ package com.black_dog20.itemgrabber.tileentity;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.Nullable;
+
 import com.black_dog20.gadgetron.capability.BeltHandler;
 import com.black_dog20.gadgetron.capability.IBeltHandler;
 import com.black_dog20.itemgrabber.reference.AntiType;
 import com.black_dog20.itemgrabber.reference.Constants;
 import com.black_dog20.itemgrabber.reference.NBTTags;
 
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.NetworkManager;
+import net.minecraft.network.play.server.SPacketUpdateTileEntity;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ITickable;
 import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class TileEntityAntiMagnet extends TileEntity implements ITickable{
 
@@ -37,6 +44,7 @@ public class TileEntityAntiMagnet extends TileEntity implements ITickable{
 		if(rangeMarker == (ranges.length)) {
 			rangeMarker = 0;
 		}
+		sendUpdates();
 	}
 	
 	public String getRangeString() {
@@ -103,16 +111,49 @@ public class TileEntityAntiMagnet extends TileEntity implements ITickable{
 	
 	@Override
 	public void readFromNBT(NBTTagCompound nbt) {
-		super.readFromNBT(nbt);
 		rangeMarker = nbt.getInteger("rangeMarker");
-		type = AntiType.valueOf(nbt.getString("type"));
+		this.type = AntiType.valueOf(nbt.getString("antiType"));
+		super.readFromNBT(nbt);
 	}
 
 	@Override
 	public NBTTagCompound writeToNBT(NBTTagCompound nbt) {
 		nbt.setInteger("rangeMarker", rangeMarker);
-		nbt.setString("type", type.toString());
+		nbt.setString("antiType", type.name());
 		return super.writeToNBT(nbt);
+	}
+	
+	@Override
+	public NBTTagCompound getUpdateTag() {
+		NBTTagCompound data = new NBTTagCompound();
+		return writeToNBT(data);
+	}
+	
+    @Nullable
+    @Override
+    public SPacketUpdateTileEntity getUpdatePacket() {
+        NBTTagCompound data = new NBTTagCompound();
+        writeToNBT(data);
+        return new SPacketUpdateTileEntity(this.pos, 1, data);
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void onDataPacket(NetworkManager networkManager, SPacketUpdateTileEntity s35PacketUpdateTileEntity) {
+        readFromNBT(s35PacketUpdateTileEntity.getNbtCompound());
+        world.markBlockRangeForRenderUpdate(this.pos, this.pos);
+        this.world.notifyBlockUpdate(this.pos, world.getBlockState(this.pos), world.getBlockState(this.pos), 3);
+    }
+    
+	public void sendUpdates() {
+		world.markBlockRangeForRenderUpdate(pos, pos);
+		world.notifyBlockUpdate(pos, getState(), getState(), 3);
+		world.scheduleBlockUpdate(pos,this.getBlockType(),0,0);
+		markDirty();
+	}
+	
+	private IBlockState getState() {
+		return world.getBlockState(pos);
 	}
 
 }
